91873fcb7bdb6c8eb118bad9fb1226844ba7c238
lee-ma <leema9321@gmail.com>
2019-08-24 22:54:11 -0600

    adjust perimeter

--- scan.py
+++ scan.py
@@ -29,7 +29,7 @@ def scan(base64_img):
 
   for contour in contours:
     perimeter = cv2.arcLength(contour, True)
-    approx = cv2.approxPolyDP(contour, 0.08*perimeter, True)
+    approx = cv2.approxPolyDP(contour, 0.02*perimeter, True)
 
     if len(approx) == 4:
       screenContours = approx
